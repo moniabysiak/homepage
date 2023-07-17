02171ab06cb2951e5a7e1e800267e7090ee7a6e2
Monika Bysiak <monika.serwin88@gmail.com>
2023-07-17 10:04:49 +0200

    homework nr 7

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert('Witaj na mojej stornie! Opowiem Ci trochę o sobie.');
+console.log('Hej! Fajnie, że tu zaglądasz.');
 const firstName = 'Monika';
 const age = 35;
 console.log(`Nazywam się ${firstName} i mam ${age} lat`);
