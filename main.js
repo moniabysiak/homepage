5e37e673fd03cbe51d099682e8847b558396eaa0
Monika Bysiak <monika.serwin88@gmail.com>
2023-07-21 10:38:16 +0200

    DOM

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ console.log('Hej! Fajnie, że tu zaglądasz.');
 const firstName = 'Monika';
 const age = 35;
 console.log(`Nazywam się ${firstName} i mam ${age} lat`);
+
+const header = document.querySelector('h1')
+
+console.log(header.style)
+
+const paragraph = document.querySelector('.section__header--paragraph--js')
+paragraph.innerHTML = 'Ten tekst widzisz dzięki JS.'
+paragraph.style.color = ('red')
