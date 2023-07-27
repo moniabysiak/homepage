38f67dc5a8867f70fbfe99a37854605fbef6b898
Monika Bysiak <monika.serwin88@gmail.com>
2023-07-27 11:40:25 +0200

    function JS

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ console.log(`Nazywam się ${firstName} i mam ${age} lat`);
 const paragraph = document.querySelector('.section__header--paragraph--js')
 paragraph.innerHTML = 'Ten tekst widzisz dzięki JS.'
 paragraph.style.color = ('red')
+
+function person(name, age, town, hobby) {
+
+console.log(`Mam na imię ${name} i mam ${age} lata.`)
+console.log(`Moje miasto to ${town}, lubię ${hobby}.`)
+}
+
+person('Monika',34, 'Kielce', 'spotkania z przyjaciółmi')
+person('Hubert', 33, 'Kielce', 'karate')
+person('Liliana', 4,'Kielce','Ju-jitsu')
+person('Aniela', 2, 'Kielce', 'plac zabaw')
