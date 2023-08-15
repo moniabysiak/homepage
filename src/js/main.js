340afc581b3cc62a116074152859f6c43184b930
Monika Bysiak <monika.serwin88@gmail.com>
2023-08-15 11:38:08 +0200

    import and export js / new  file scss section

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
+import { paragraph } from "./paragraph";
+
 console.log('Hej! Fajnie, że tu zaglądasz.');
 const firstName = 'Monika';
 const age = 35;
 console.log(`Nazywam się ${firstName} i mam ${age} lat`);
 
-const paragraph = document.querySelector('.section__header--paragraph--js')
-paragraph.innerHTML = 'Ten tekst widzisz dzięki JS.'
-paragraph.style.color = ('red')
+
 
 function person(name, age, town, hobby) {
 
---
+++ src/js/paragraph.js
@@ -0,0 +1,5 @@
+const paragraph = document.querySelector('.section__header--paragraph--js')
+paragraph.innerHTML = 'Ten tekst widzisz dzięki JS.'
+paragraph.style.color = ('red')
+
+export {paragraph};
