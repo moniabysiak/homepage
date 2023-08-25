567a3d45998a539ab7091896ad2f758116bbfbf7
Monika Bysiak <monika.serwin88@gmail.com>
2023-08-25 12:41:49 +0200

    debuging

--- src/js/loops.js
+++ src/js/loops.js
@@ -2,10 +2,21 @@ for (let i=0; i < 3; i++) {
     console.log(`${i+1} wykonanie pętli`)
 }
 
-const elements = ['❤', '😊', '😍', '🎀']
-for (let i=0; i<elements.length; i++) {
-    console.log(`Index ${i} Emoji: ${elements[i]}`)
+let myNumber = 13;
+
+const myFunction = (a) => {
+    const b = a * 3;
+    const c = b * 7;
+    return b * c;
 }
+
+const elements = ['❤', '😊', '😍', '🎀']
+for (let i = 0; i < elements.length; i++) {
+    myNumber = myNumber * (i+1);
+    console.log(`Index ${i} Emoji: ${elements[i]}`)
+    myFunction(i);
+}
+
 
 let n=3;
 while (n <= 9) {
