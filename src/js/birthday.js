acdaa2d37e64e6fff0a5c5888049e8762ff6b249
Monika Bysiak <monika.serwin88@gmail.com>
2023-09-11 13:43:13 +0200

    birthday hours

---
+++ src/js/birthday.js
@@ -0,0 +1,12 @@
+import JSConfetti from 'js-confetti'
+
+const jsConfetti = new JSConfetti()
+
+const { getHours, getMinutes } = require("date-fns");
+
+let date = new Date().toISOString().slice(0, 10);
+
+setInterval(() => {
+    if (getHours(date) == getMinutes(date))
+    jsConfetti.addConfetti()
+}, 1000*60)
